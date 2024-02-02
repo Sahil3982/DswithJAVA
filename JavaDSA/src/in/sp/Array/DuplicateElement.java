d9dbce905601b8f96309ddcb7669b928ed06ec0d
sahil3982 <113926133+Sahil3982@users.noreply.github.com>
2024-02-02 18:24:11 +0530

    Jan'02

--- JavaDSA/src/in/sp/Array/DuplicateElement.java
+++ JavaDSA/src/in/sp/Array/DuplicateElement.java
@@ -1,4 +1,18 @@
 package in.sp.Array;
 
 public class DuplicateElement {
+    public static void main(String[] args) {
+        int[] num = {10, 15, 13,15, 14, 28, 47, 24, 12, 9, 9};
+        for (int i = 0; i < num.length - 1; i++) {
+            for (int j = i + 1; j < num.length; j++) {
+                if (num[i] == num[j]) {
+                    System.out.println(num[i]);
+                    break;
+                }
+            }
+
+        }
+    }
+
 }
+
